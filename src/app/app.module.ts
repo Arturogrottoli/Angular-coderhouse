be0d02ac8dab16443055e402ecf143e161dddad5
Arturo <arturogrottoli@gmail.com>
2022-09-23 14:49:46 -0300

    Primer entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { MainComponent } from './componentes/main/main.component';
-import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
+import { MainComponent } from './components/main/main.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
